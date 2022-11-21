0139ed8419093b0a4810e9949ba50499c9a725ec
kevinsamsi <kevinsamsi@uchicago.edu>
2022-11-21 13:12:11 -0600

    Kevin

---
+++ FinalProject.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(fredr)
+#fredr_set_key("a276ace28f00c2ba0f9bfa14ea5f2289")
+
+gdp_indonesia <- fredr(series_id = "NGDPRSAXDCIDQ",
+              observation_start = as.Date("2014-01-01"),
+              observation_end   = as.Date("2022-07-01"))
+#Reference: https://fred.stlouisfed.org/series/NGDPRSAXDCIDQ
+
+gdp_unitedstates <- fredr(series_id = "GDPC1",
+                          observation_start = as.Date("2014-01-01"),
+                          observation_end   = as.Date("2022-07-01"))
+#Reference: https://fred.stlouisfed.org/series/GDPC1
+
+#Convert Indonesia local currency to Billions of USD
+gdp_indonesia$value_2 <- gdp_indonesia$value * 1000000 / 15735.60 / 1000000000
+
+CPI_indonesia <- fredr(series_id = "FPCPITOTLZGIDN",
+                       observation_start = as.Date("2014-01-01"),
+                       observation_end   = as.Date("2022-01-01"))
+#Reference: https://fred.stlouisfed.org/series/FPCPITOTLZGIDN
+
+CPI_unitedstates <- fredr(series_id = "FPCPITOTLZGUSA",
+                       observation_start = as.Date("2014-01-01"),
+                       observation_end   = as.Date("2022-01-01"))
+#Reference: https://fred.stlouisfed.org/series/FPCPITOTLZGUSA
+
+interestrate_unitedstates <- fredr(series_id = "FEDFUNDS",
+                                observation_start = as.Date("2014-01-01"),
+                                observation_end   = as.Date("2022-01-01"))
+#Reference: https://fred.stlouisfed.org/series/FEDFUNDS
