d0613c507270236014d262d978e28eb8ffc46408
citraagustina <90005502+cicit28@users.noreply.github.com>
2022-11-29 14:37:25 -0600

    Update FinalProject.R

--- FinalProject.R
+++ FinalProject.R
@@ -5,7 +5,10 @@ library(dplyr)
 library(lubridate)
 library(zoo)
 library(dplyr)
+<<<<<<< Updated upstream
 library(magrittr)
+=======
+>>>>>>> Stashed changes
 
 fredr_set_key("a276ace28f00c2ba0f9bfa14ea5f2289")
 
@@ -27,8 +30,33 @@ create_df <- function(a,b,c,d){
 prod_index_ind <- create_df("IDNPRMNTO01IXOBM", growth_prod_ind, log_prod_ind, prod_index_ind)
 #Reference: https://fred.stlouisfed.org/series/PRMNTO01IDQ661N
 
+<<<<<<< Updated upstream
   
 ##Production Index United States
+=======
+prod_index_ind <- prod_index_ind %>% 
+  group_by(month=month(date)) %>%
+  arrange(date) %>%
+  mutate(growth_prod_ind=((value/lag(value,1))-1)*100)%>%
+  ungroup() %>% arrange(date)
+
+prod_index_ind$log_prod_ind<-log(prod_index_ind$value)
+colnames(prod_index_ind)[3] <- "prod_index_ind"
+prod_index_ind$date <- format(as.Date(prod_index_ind$date), "%Y-%m")
+prod_index_ind <- prod_index_ind[, c('date', 'prod_index_ind', 'growth_prod_ind', 'log_prod_ind')]
+prod_index_ind = prod_index_ind[13:99,]
+
+##Production Index United States
+prod_index_us <- fredr(series_id = "INDPRO",
+                       observation_start = as.Date("2014-01-01"),
+                       observation_end   = as.Date("2022-09-01"))
+#Reference: https://fred.stlouisfed.org/series/PRMNTO01USQ661N
+
+prod_index_us = prod_index_us %>% group_by(month=month(date)) %>%
+  arrange(date) %>%
+  mutate(growth_prod_us=((value/lag(value,1))-1)*100)%>%
+  ungroup() %>% arrange(date)
+>>>>>>> Stashed changes
 
 prod_index_us <- create_df("INDPRO", growth_prod_us, log_prod_us, prod_index_us)
 #Reference: https://fred.stlouisfed.org/series/PRMNTO01USQ661N
@@ -37,8 +65,21 @@ prod_index_us <- create_df("INDPRO", growth_prod_us, log_prod_us, prod_index_us)
 # Dependent Variable-CPI data
 ##CPI Indonesia
 
+<<<<<<< Updated upstream
 CPI_Indonesia <- create_df("IDNCPIALLMINMEI", inflation_ind, log_inflation_ind, CPI_ind)
 #Reference: https://fred.stlouisfed.org/series/IDNCPIALLMINMEI
+=======
+CPI_indonesia = CPI_indonesia %>% group_by(month=month(date)) %>%
+  arrange(date) %>%
+  mutate(inflation_ind=((value/lag(value,1))-1)*100)%>%
+  ungroup() %>% arrange(date)
+
+CPI_indonesia$log_inflation_ind<-log(CPI_indonesia$value)
+colnames(CPI_indonesia)[3] <- "CPI_ind"
+CPI_indonesia$date <- format(as.Date(CPI_indonesia$date), "%Y-%m")
+CPI_indonesia = subset(CPI_indonesia, select = c(date, CPI_ind ,inflation_ind,log_inflation_ind))
+CPI_indonesia = CPI_indonesia[13:105,]
+>>>>>>> Stashed changes
 
 
 ##CPI United States
@@ -47,14 +88,34 @@ CPI_unitedstates <- create_df("USACPIALLMINMEI", inflation_us, log_inflation_us,
 
 
 #Dependent Variable - Credit Growth
+##Credit Indonesia
 
 
+<<<<<<< Updated upstream
 ##Bank Credit Indonesia
 
 
 #Bank Credit US
 credit_us <- create_df("LOANINV", growth_credit_us, log_credit_us, credit_us)
 #Reference?????
+=======
+#Credit US
+credit_us<-fredr(series_id = "LOANINV",
+                 observation_start = as.Date("2014-01-01"),
+                 observation_end   = as.Date("2022-09-01"))
+
+credit_us = credit_us %>% group_by(month=month(date)) %>%
+  arrange(date) %>%
+  mutate(growth_credit_us=((value/lag(value,1))-1)*100)%>%
+  ungroup() %>% arrange(date)
+
+credit_us$log_credit_us<-log(credit_us$value)
+colnames(credit_us)[3] <- "credit_us"
+credit_us$date <- format(as.Date(credit_us$date), "%Y-%m")
+credit_us = subset(credit_us, select = c(date, credit_us ,growth_credit_us,log_credit_us))
+credit_us = credit_us[13:105,]
+
+>>>>>>> Stashed changes
 
 #Independent Variable-Policy rate
 ##Policy rate Indonesia
@@ -123,7 +184,6 @@ data_us_ts<-as.ts(data_us)
 #run model ARDL each country (STEP1)
 ##run ARDL model Indonesia - Credit Growth
 library(dynlm)
-
 ##run ARDL model Indonesia - Inflation
 model_1_ind_inflation<-dynlm(d(log_inflation_ind,1)~L(BI_rate,1)+L(log_uncertainty,1)+L(log_er_ind,1),data=data_ind_ts)
 summary(model_1_ind_inflation)
@@ -157,7 +217,7 @@ summary(model_2_us_inflation)
 model_3_us_inflation<-dynlm(d(log_inflation_us,1)~L(FFR,1)+L(log_uncertainty,1)+L(log_er_us,1)+L((FFR*log_uncertainty),1)+L((FFR*log_uncertainty*dummy),1),data=data_us_ts)
 summary(model_3_us_inflation)
 
-##run ARDL model Indonesia - Output Growth
+##run ARDL model - Output Growth
 model_1_us_output<-dynlm(d(log_prod_us,1)~L(FFR,1)+L(log_uncertainty,1)+L(log_er_us,1),data=data_us_ts)
 summary(model_1_us_output)
 model_2_us_output<-dynlm(d(log_prod_us,1)~L(FFR,1)+L(log_uncertainty,1)+L(log_er_us,1)+L((FFR*log_uncertainty),1),data=data_us_ts)
